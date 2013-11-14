1ddf9e2c7137ad75b574438d8e2b6606fd48db30
skoeba_steve <janssenman@gmail.com>
2013-11-14 20:44:52 +0100

    finished basic scrumboard, added charts

--- js/functions.js
+++ js/functions.js
@@ -1,40 +1,66 @@
 $(document).ready(function(){
 
-	if (localStorage.getItem("tasks") !== null) {
-		updateTables();
-	}
-	var tasks = [];
-	var taskList = [];
-
-	function updateTables(){
-		$("#todo>tbody>tr").remove();
-		taskList = JSON.parse(localStorage["tasks"]);
-		console.log(taskList);
-		console.log(taskList.length);
-		$.each(taskList, function(key, value){
-			if(value.done=="yes"){
-				$("#todo").append("<tr><td>"+value.name+"</td><td>"+value.date+"</td><td><input class=\"check\" type=\"checkbox\" checked/></td></tr>");
-			}
-			else{
-				$("#todo").append("<tr><td>"+value.name+"</td><td>"+value.date+"</td><td><input class=\"check\" type=\"checkbox\"/></td></tr>");
-			}
-			//console.log(value.date);
-		});
-	}	 
-
-	$('button[name="save"]').click(function(){
-		var name = $('input[name="taskname"]').val();
-		var date = $('input[name="date"]').val();
-		var task = {name:name,date:date,done:"no"};
-		tasks.push(task);
-		localStorage["tasks"] = JSON.stringify(tasks);
-		updateTables();
-		var loading_dialog = $('#myModal');
-		loading_dialog.modal('hide');
-	});
-
-	$('.check').click(function(){
+	$(document).on('click', '.move-item', function(){
+		var task = $(this).prev().html();
+		var rowID = $(this).closest('div').attr('id');
+		rowID++;
+		var currentListItem = $(this).closest('li');
+		if(rowID==4){
+			bootbox.alert("Your team members have been informed about the task you completed. Just sit back, drink some coffee and wait for their review!", function() {
+  				currentListItem.addClass('btn-info');
+  				var val = $('#messageBubble').html();
+  				val++;
+  				$('#messageBubble').html(val);
+			});
+		}
+		else{
+			$('#'+rowID).children('ul').append($('<li class="list-group-item"><span class="task">'+task+'</span><span class="glyphicon glyphicon-chevron-right pull-right move-item"></span></li>'));	
+			$(currentListItem).remove();
+		}
 		
 	});
+
+	var burndownData = {
+		labels : ["10-nov","11-nov","12-nov","13-nov","14-nov","15-nov"],
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				data : [100,80,60,40,20,0]
+			},
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,1)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				data : [100,85,70,45,15,0]
+			}
+		]
+	}
+
+	var teamEffortData = [
+		{
+			value: 30,
+			color:"#F38630"
+		},
+		{
+			value : 50,
+			color : "#E0E4CC"
+		},
+		{
+			value : 100,
+			color : "#69D2E7"
+		}			
+	]
+
+    var burnDown = new Chart($("#burnDown").get(0).getContext("2d")).Line(burndownData, function(){
+    	bezierCurve : false
+    	});
+    var teamEffort = new Chart($("#teamEffort").get(0).getContext("2d")).Pie(teamEffortData, function(){
+
+    	});
+    
 });
 
